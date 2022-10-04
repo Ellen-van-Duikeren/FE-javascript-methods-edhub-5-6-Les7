0e03ed81ef67b56b8039584f74a8788a9a8a5c2d
Ellen van Duikeren <e.vanduikeren@gmail.com>
2022-10-04 11:55:30 +0200

    Made exercises with math and strings

--- math-methods.js
+++ math-methods.js
@@ -1,3 +1,5 @@
+//done & checked
+
 /* Opdracht 0: voorbeeld */
 // Log het cijfer PI in de terminal
 // ---- Verwachte uitkomst: 3.141592653589793
@@ -8,18 +10,21 @@ console.log(Math.PI);
 // Bereken de omtrek van een cirkel met een diameter van 4 en log dit in de terminal. De eenheid (cm, meters, etc. maakt niet uit).
 // Tip: de formule is omtrek = diameter * pi
 // ---- Verwachte uitkomst: 12.566370614359172
-
+console.log("\nOpdracht 1")
+console.log(4 * Math.PI);
 
 /* Opdracht 2: */
 // Bereken de omtrek van een cirkel met een diameter van 12 en rond dit af naar een heel getal. Log dit in de terminal.
 // ---- Verwachte uitkomst: 38
-
+console.log("\nOpdracht 2")
+console.log(Math.round(12 * Math.PI));
 
 
 /* Opdracht 3: */
 // Bereken de wortel van 9 en log dit in de terminal
 // ---- Verwachte uitkomst: 3
-
+console.log("\nOpdracht 3")
+console.log(Math.sqrt(9));
 
 
 /* Opdracht 4: */
@@ -28,3 +33,13 @@ console.log(Math.PI);
 // * is het willekeurige getal kleiner dan 0.5 ? Log dan het getal met "Het glas is half leeg" in de terminal
 // Tip: iedere keer als je het script opnieuw draait (wel even een extra spatie toevoegen o.i.d.) is dit getal anders ;)
 // ---- Verwachte uitkomst: Bijbehorende zin afhankelijk van de uitkomst
+console.log("\nOpdracht 4")
+const number = Math.random()
+console.log(number);
+if (number > 0.5) {
+    console.log("Het glas is halfvol");
+} else if (number < 0.5) {
+    console.log("Het glas is half leeg");
+} else {
+    console.log("Het glas is leeg");
+}
--- string-methods.js
+++ string-methods.js
@@ -1,3 +1,5 @@
+// done & checked
+
 /* Opdracht 0: voorbeeld */
 // Check de lengte van onderstaande string en log dit in de terminal
 // ---- Verwachte uitkomst: 10
@@ -13,6 +15,8 @@ console.log(word.length);
 const fruit = "Banaan";
 const vegetable = "Aardappel";
 
+console.log("\nOpdracht 1")
+console.log(fruit.length > vegetable.length);
 
 
 /* Opdracht 2 */
@@ -20,7 +24,9 @@ const vegetable = "Aardappel";
 //  Tip: bedenk 1 manier die voor beide woorden zou werken.
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
-
+console.log("\nOpdracht 2")
+console.log(fruit.substring(fruit.length-1));
+console.log(vegetable.substring(vegetable.length-1));
 
 
 /* Opdracht 3 */
@@ -29,6 +35,8 @@ const vegetable = "Aardappel";
 
 const userInput = "  De kat krabt de krullen van de trap ";
 
+console.log("\nOpdracht 3")
+console.log(userInput.trim());
 
 
 /* Opdracht 4 */
@@ -37,12 +45,17 @@ const userInput = "  De kat krabt de krullen van de trap ";
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
 
+console.log("\nOpdracht 4")
+console.log(story.includes("Fantastisch"));
 
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
-
+console.log("\nOpdracht 5")
+index  = story.indexOf(".")
+const sentence = story.substring(0, index + 1);
+console.log(sentence);
 
 
 /* Opdracht 6 */
@@ -51,10 +64,15 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 // * Vervang de punt voor een komma
 // Log dit in de terminal.
 // ---- Verwachte uitkomst: "€35,59"
+console.log("\nOpdracht 6")
 
 const price = "$35.59";
+price[0] = "€"; // evt eerst met indexOf("$")
+price[3] = "."
+console.log(price);
 
-
+console.log("\nOpdracht 6 with replace")
+console.log(price.replace("$", "€").replace(".", ","));
 
 
 
